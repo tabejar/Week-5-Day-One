7018cc6d86d244f0560b364e2bfbc6ceb42d1141
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-09-21 17:28:50 +0000

    dictionaries

--- main.py
+++ main.py
@@ -137,14 +137,21 @@ print(result)
 #############################################next week ################################################
 #dictionaries and  sets
 
-# dictionaries
+# dictionaries:
 # another data structure that stores data that can be unstructured
-# employee = {'first_name': 'steven',
-#             'last_name': 'pretti',
-#              'weight': 37.2,
-#               'size': 5.77
-#              'height:' 6}
-# print(employee['weight'])
+employee = {'first_name': 'steven',
+            'last_name': 'pretti',
+             'weight': 37.2,
+              'size': 5.77,
+             'height': 6}
+print(employee['height'])
+
+warrior = {'made_by': 'hbomax',
+          'release_date': 2018,
+          'budget': '24million',
+          'rotten_tomatoes_scores': 'fresh'}
+print(warrior['release_date'])
+print(warrior['budget'])
 
 #challenge 1
 # Create a dictionary called my_dict that stores the following information about a person:
