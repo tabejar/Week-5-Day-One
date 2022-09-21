fa3f4186644bd71ad88ca22ad5120454c14cc8b2
Thomas Bejar <113930376+tabejar@users.noreply.github.com>
2022-09-21 16:53:49 +0000

    counted variables and made strings into lists

--- main.py
+++ main.py
@@ -7,36 +7,47 @@
 #example
 coordinates = (4, 5)
 # get the first element in the above tuple
-# print(coordinates[1])
+print(coordinates[1])
 # lets make this a multidimensional array
 coordindates2 = [(4, 5), (6, 7), (80, 34)]
 #get the second element of the second item in coordinates2
-
+print(coordindates2[2][1])
 # we use parentheses not [] in tuples
 my_tuple1 = (1, 2, 3, 4)
-
+print(my_tuple1[2])
 # get the second item in the tuple1 above
 my_tuple = (1, 2, (10, 20), 4)
 
 # get the second item in the 3rd item above
 #hint multidimensional array thinking
-
+print(my_tuple[2][1])
 # place these numbers in separate variables from the tuple below
 t = (1, 2, 3)
-
+x, y, z = t
+print(x,y,z)
 #get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
+print(len(my_tuple))
+my_tuple3 = (1,2,3,3,4,5,6,7,7,8,9,10)
+print(my_tuple3.count(2))
+my_tuple4 = ('a','a','a','b','b')
+print(my_tuple4.count('b'))
 
 # Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
 
 my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+print(my_tuple.count(2))
 
 # Convert the following tuple to a list, and store it in a variable called my_list.
 
 my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
+list = list(my_tuple)
+print(list)
 
 #Extract the elements of the following tuple into four variables: a, b, c, d
 
 my_tuple = (1, 2, 3, 4)
+a,b,c,d = my_tuple
+print(a,b,c,d)
 
 
 #################################################sets######################################################
